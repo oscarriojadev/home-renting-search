d4ac6c1e1cc726ed9dc7adc0fc3fdce9f9371c9d
Óscar Rioja <70517242+oscarriojadev@users.noreply.github.com>
2025-03-30 21:02:23 +0200

    Update home-renting-search-app_01.py

--- home-renting-search-app_01.py
+++ home-renting-search-app_01.py
@@ -4,9 +4,8 @@ import platform
 import sys
 import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-service = Service(ChromeDriverManager(version='115.0.5790.170').install())
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
